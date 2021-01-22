39081737bbfaaa14eb70e76e78ef49548b705670
Dan Isla <dan.isla@gmail.com>
2021-01-22 05:42:48 +0000

    [blender] upgrade to 2.91 with ubutnu focal base image, GPU rendering and CUDA kernel caching support

--- blender/images/blender/start_blender.sh
+++ blender/images/blender/start_blender.sh
@@ -1,8 +1,15 @@
-#!/bin/sh
+#!/usr/bin/bash
+
+# Symlink cached blender kernels
+if [[ -d /opt/blender/cache/cycles/kernels ]]; then
+    echo "INFO: copying cached kernels from /opt/blender/cache/cycles/kernels/"
+    mkdir -p ${HOME}/.cache/cycles/kernels/
+    ls /opt/blender/cache/cycles/kernels/* | xargs -I {} ln -sf {} ${HOME}/.cache/cycles/kernels/
+fi
 
 # Disable window manager ALT+LEFT_MOUSE to move window action,
 # This is the orbit action in Maya interaction mode.
 xfconf-query -c xfwm4 -p /general/easy_click -s none
 
 # Start Blender in fullscreen mode
-exec /usr/bin/blender -W
+exec /usr/bin/blender -W $@
